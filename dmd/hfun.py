189f8a9f300c8aae4acc59480996367f895d7ecc
Benli11 <nbe@st-andrews.ac.uk>
2016-04-11 15:44:04 -0700

    help function

---
+++ dmd/hfun.py
@@ -0,0 +1,11 @@
+"""
+Functions to increase the performance
+"""
+
+#matrix transpose for real matricies
+def rT(A): 
+    return A.T
+    
+#matrix transpose for complex matricies
+def cT(A): 
+    return A.conj().T   
