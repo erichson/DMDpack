30f0805c509c38149b7a004053bec3ddea6eecb6
benli11 <benli11@users.noreply.github.com>
2017-07-20 00:40:27 -0700

    tests for rdmd

--- tests/tests.py
+++ tests/tests.py
@@ -114,7 +114,7 @@ class test_dmd(TestCase):
         F2 = ( (1./np.cosh(X)) * np.tanh(X)) *(2.*np.exp(1j*2.8*T))
         A = np.array((F1+F2).T, order='C')
         
-        Fmodes, b, V, omega = dmd(A, k=2, modes='standard', svd='partial', return_amplitudes=True, return_vandermonde=True)
+        Fmodes, b, V, omega = dmd(A, k=2, modes='standard', svd='truncated', return_amplitudes=True, return_vandermonde=True)
         Atilde = Fmodes.dot( np.dot(np.diag(b), V))        
         assert np.allclose(A, Atilde, atol_float64)   
 
@@ -133,7 +133,7 @@ class test_dmd(TestCase):
         Atilde = Fmodes.dot( np.dot(np.diag(b), V))        
         assert np.allclose(A, Atilde, atol_float64)  
 
-    def test_rdmd_standard(self):
+    def test_dmd_rsvd_standard(self):
         # Define time and space discretizations
         x=np.linspace( -10, 10, 100)
         t=np.linspace(0, 8*np.pi , 60) 
@@ -148,7 +148,7 @@ class test_dmd(TestCase):
         Atilde = Fmodes.dot( np.dot(np.diag(b), V))        
         assert np.allclose(A, Atilde, atol_float64) 
 
-    def test_rdmd_exact(self):
+    def test_dmd_rsvd_exact(self):
         # Define time and space discretizations
         x=np.linspace( -10, 10, 100)
         t=np.linspace(0, 8*np.pi , 60) 
@@ -163,7 +163,9 @@ class test_dmd(TestCase):
         Atilde = Fmodes.dot( np.dot(np.diag(b), V))        
         assert np.allclose(A, Atilde, atol_float64) 
      
-    def test_cdmd_exact(self):
+        
+        
+    def test_cdmd(self):
         # Define time and space discretizations
         x=np.linspace( -10, 10, 100)
         t=np.linspace(0, 8*np.pi , 60) 
@@ -179,26 +181,62 @@ class test_dmd(TestCase):
         assert np.allclose(A, Atilde, atol_float64) 
 
 
+
+    def test_rdmd_uniform(self):
+        # Define time and space discretizations
+        x=np.linspace( -10, 10, 100)
+        t=np.linspace(0, 8*np.pi , 60) 
+        dt=t[2]-t[1]
+        X, T = np.meshgrid(x,t)
+        # Create two patio-temporal patterns
+        F1 = 0.5* np.cos(X)*(1.+0.* T)
+        F2 = ( (1./np.cosh(X)) * np.tanh(X)) *(2.*np.exp(1j*2.8*T))
+        A = np.array((F1+F2).T, order='C')
+        
+        Fmodes, b, V, omega = rdmd(A, k=2, p=10, q=2, sdist='uniform', return_amplitudes=True, return_vandermonde=True)
+        Atilde = Fmodes.dot( np.dot(np.diag(b), V))        
+        assert np.allclose(A, Atilde, atol_float64) 
+
+
+    def test_rdmd_normal(self):
+        # Define time and space discretizations
+        x=np.linspace( -10, 10, 100)
+        t=np.linspace(0, 8*np.pi , 60) 
+        dt=t[2]-t[1]
+        X, T = np.meshgrid(x,t)
+        # Create two patio-temporal patterns
+        F1 = 0.5* np.cos(X)*(1.+0.* T)
+        F2 = ( (1./np.cosh(X)) * np.tanh(X)) *(2.*np.exp(1j*2.8*T))
+        A = np.array((F1+F2).T, order='C')
+        
+        Fmodes, b, V, omega = rdmd(A, k=2, p=10, q=2, sdist='normal',  return_amplitudes=True, return_vandermonde=True)
+        Atilde = Fmodes.dot( np.dot(np.diag(b), V))        
+        assert np.allclose(A, Atilde, atol_float64) 
+
+    def test_rdmd_normal_complex64(self):
+        # Define time and space discretizations
+        x=np.linspace( -10, 10, 100)
+        t=np.linspace(0, 8*np.pi , 60) 
+        dt=t[2]-t[1]
+        X, T = np.meshgrid(x,t)
+        # Create two patio-temporal patterns
+        F1 = 0.5* np.cos(X)*(1.+0.* T)
+        F2 = ( (1./np.cosh(X)) * np.tanh(X)) *(2.*np.exp(1j*2.8*T))
+        A = np.array((F1+F2).T,  np.complex64, order='C')
+        
+        Fmodes, b, V, omega = rdmd(A, k=2, p=10, q=2, sdist='normal',  return_amplitudes=True, return_vandermonde=True)
+        Atilde = Fmodes.dot( np.dot(np.diag(b), V))        
+        assert np.allclose(A, Atilde, atol_float32) 
+
+
+
 #
 #******************************************************************************
 #
         
 def suite():
     s = TestSuite()
-    s.addTest(test_rlinalg('test_rsvd_float32'))
-    s.addTest(test_rlinalg('test_rsvd_float64'))
-    s.addTest(test_rlinalg('test_rsvd_complex64'))
-    s.addTest(test_rlinalg('test_rsvd_complex128'))
-    s.addTest(test_rlinalg('test_rsvdf_float32'))
-    s.addTest(test_rlinalg('test_rsvdf_float64'))
-    s.addTest(test_rlinalg('test_rsvdf_complex64'))
-    s.addTest(test_rlinalg('test_rsvdf_complex128'))
-    
-    s.addTest(test_dmd('test_dmd_standard'))
-    s.addTest(test_dmd('test_dmd_exact'))
-    s.addTest(test_dmd('test_rdmd_standard'))
-    s.addTest(test_dmd('test_rdmd_exact'))
-    s.addTest(test_dmd('test_cdmd_exact'))
+
 
     
     return s
