49767598169b1c7526ac8976128130bd6da2fabd
benli11 <benli11@users.noreply.github.com>
2017-07-20 00:40:04 -0700

    rdmd added

--- dmd/__init__.py
+++ dmd/__init__.py
@@ -1,8 +1,9 @@
 from .dmd import dmd
+from .rdmd import rdmd
 from .cdmd import cdmd
 from .tdmd import tdmd
 from .rsvd import rsvd
 from .hfun import rT, cT
 
 
-__all__ = ["rsvd", "dmd", "cdmd"]
+__all__ = ["rsvd", "dmd", "rdmd", "cdmd"]
